3a0a4d158d1158a16c7e279326d64b42d1113e8e
CWH <aq3aq4@gmail.com>
2017-08-27 17:10:56 +0900

    주석 생성

--- src/com/jodu/guice/usemethod/config/InjectConfig.java
+++ src/com/jodu/guice/usemethod/config/InjectConfig.java
@@ -7,7 +7,9 @@ import com.jodu.guice.usemethod.service.impl.CoffieOrderService;
 import com.jodu.guice.usemethod.service.impl.PizzaOrderService;
 
 public class InjectConfig extends AbstractModule {
-
+	/**
+	 * 의존성 주입 설정 부분 
+	 */
 	@Override
 	protected void configure() {
 		bind(OrderService.class).annotatedWith(Names.named("pizza")).to(PizzaOrderService.class);
